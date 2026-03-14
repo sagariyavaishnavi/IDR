efbb31451cdfb56f7b59ba6c76520203f5ac2c42
sagariyavaishnavi <sagariyavaishnavi@gmail.com>
2026-03-15 03:20:03 +0530

    fix the ui of mobile

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('active');
+            } else {
+                entry.target.classList.remove('active');
             }
         });
     }, {
